4fbb9e0082ea5108dc1727a04bc286d47457d94d
unknown <borshadevi27@gmail.com>
2024-11-21 23:18:51 +0600

    added items

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 
-const uri = `mongodb+srv://mustofaKebab:nttgpDKueCAFA747@cluster0.0afhd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// const uri = `mongodb+srv://mustofaKebab:nttgpDKueCAFA747@cluster0.0afhd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const uri='mongodb+srv://Mustafa:tnS42aC0hF6GK5hG@cluster0.uqcmivv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -30,21 +31,27 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    const menuCollection = client.db('mustofa').collection('menu');
-    const orderCollection = client.db('mustofa').collection('order');
+    const menuCollection = client.db('Mustafa').collection('menu'); //mustofa
+    const orderCollection = client.db('Mustafa').collection('order');
 
+
+
+          //  Get all ordersMenu data 
+          app.get ('/getAllOrdersMenu', async(req,res)=>{
+            const orderMenu=await orderCollection.find().toArray()
+            res.send(orderMenu)
+          })
           // Get all menu data form db 
           app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
-            console.log(result)
             res.send(result);
         })
 
+        //  ordersMenu 
         app.post('/ordersMenu',async(req,res)=>{
           try {
             const order = req.body;
             const orderMenu = await orderCollection.insertOne(order);
-            console.log('Order saved:', orderMenu);
             res.send(orderMenu);
           } catch (error) {
             console.error('Error while saving order:', error);
